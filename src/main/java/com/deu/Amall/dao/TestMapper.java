8f0c2c9c09f57d7813d8315d06aef1f6f4e4706d
Lee-Kang-Min-123 <dlrkdals1025@naver.com>
2020-11-23 15:29:12 +0900

    최초 커밋

---
+++ src/main/java/com/deu/Amall/dao/TestMapper.java
@@ -0,0 +1,12 @@
+package com.deu.Amall.dao;
+
+//import java.util.HashMap;
+import java.util.List;
+
+import com.deu.Amall.util.PubMap;
+
+public interface TestMapper {
+
+    List<PubMap> selectTestData();
+
+}
