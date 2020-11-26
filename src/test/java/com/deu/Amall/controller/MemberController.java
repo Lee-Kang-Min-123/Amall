a52657062c411cadd13611472fdf4afa89d35b19
SAMSUNG <SAMSUNG@DESKTOP-5ROL0KE>
2020-11-26 14:50:42 +0900

    contro

--- src/test/java/com/deu/Amall/controller/MemberController.java
+++ src/test/java/com/deu/Amall/controller/MemberController.java
@@ -2,15 +2,21 @@
 
 package com.deu.Amall.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.deu.Amall.service.MemberService;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 public class MemberController {
+	
+	@Autowired
+	private MemberService memberservice;
 	
 	//회원가입 페이지 이동
 	@RequestMapping(value = "join", method = RequestMethod.GET)
