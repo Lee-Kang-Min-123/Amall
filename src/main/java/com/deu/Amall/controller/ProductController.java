1c2a78d01689eb3ab48ae4fc749696c1f5810262
Wonhyeok2 <zja12346@naver.com>
2020-11-30 17:06:14 +0900

    er

--- src/main/java/com/deu/Amall/controller/ProductController.java
+++ src/main/java/com/deu/Amall/controller/ProductController.java
@@ -1,38 +1,25 @@
 package com.deu.Amall.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.deu.Amall.dao.TestMapper;
 import com.deu.Amall.service.ProductService;
-import com.deu.Amall.util.PubMap;
+
+import lombok.extern.log4j.Log4j;
 
 @Controller
-
+@Log4j
 public class ProductController{
 	
-	@Inject
+	@Autowired
 	ProductService productService;
 	
 	//1. 상품 전체 목록
 	@RequestMapping("/productlist")
 	public ModelAndView list(ModelAndView mav) {
+		log.info("1234");
 		mav.setViewName("Productlist");
 		mav.addObject("list", productService.getAllProducts());
 		return mav;
---
+++ src/main/webapp/WEB-INF/views/Productlist.jsp
@@ -0,0 +1,32 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>상품</title>
+
+</head>
+<body>
+	<h2>Hello</h2>
+ 	<form action="paymentlist" method="post">
+ 		<input type ="submit" value="All payment">
+ 		</form>
+ 		
+  <table border="2px">
+  <tr>
+  	<th width="100">Product ID</th>
+  	<th width="150">Product Type</th>
+  	<th width="200">Product Name</th>
+  	<th width="400">Explanation</th>
+  	<th width="150">Price</th>
+  	<th width="100">Inventory</th>
+  	</tr>
+  	</table>
+  	
+  	<form action="updatelist" method="post">
+  		<input type ="submit" value="creat">
+  	</form>
+</body>
+</html>
