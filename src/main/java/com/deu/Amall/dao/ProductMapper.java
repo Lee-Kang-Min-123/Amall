b38de86be489b33abab95429372c221a01872626
Wonhyeok2 <zja12346@naver.com>
2020-11-30 14:56:30 +0900

    w

--- src/main/java/com/deu/Amall/dao/ProductMapper.java
+++ src/main/java/com/deu/Amall/dao/ProductMapper.java
@@ -7,6 +7,8 @@ import com.deu.Amall.util.PubMap;
 public interface ProductMapper {
 	
 	public List<PubMap> getAllProducts();
+	public void insert(com.deu.Amall.domain.ProductVO productVO);
+	public PubMap getProduct(int productId);
 	
 	/*
 	@Select("SELECT COUNT(*) totalCnt FROM product")
